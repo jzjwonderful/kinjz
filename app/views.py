8de1de54ba8f1862226210227678a0a08d655d52
jared kin <jared_kin@qq.com>
2017-04-03 16:41:55 +0800

    add comment

--- app/views.py
+++ app/views.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" import module or package"""
 from app import app
-from flask import jsonify
-from flask import request,render_template
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
+    """ setup index route """
     #return jsonify({'name':'jared','email':'test@qq.com'})
     return render_template("index.html")
 
+
 @app.route('/login')
 def login():
+    """ setup login route """
     return "cann't find login page"
