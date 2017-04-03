311b2f087802597b46624f1836f9d9302f4cab7e
www <www@USA.(none)>
2017-04-03 01:34:30 -0400

    project ini

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from app import app
+from flask import jsonify
+from flask import request
+
+@app.route('/')
+def index():
+    print(request.__dict__)
+    return jsonify({'name':'jared','email':'test@qq.com'})
