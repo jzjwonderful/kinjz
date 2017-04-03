4680a7c9fb2a0def32e58a45cc25c8a91b439dc4
Jared Kin <Jared Kin>
2017-04-03 15:46:44 +0800

    add templates

--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
 from app import app
 from flask import jsonify
-from flask import request
+from flask import request,render_template
 
 @app.route('/')
 def index():
-    print(request.__dict__)
-    return jsonify({'name':'jared','email':'test@qq.com'})
+    #return jsonify({'name':'jared','email':'test@qq.com'})
+    return render_template("index.html")
 
 @app.route('login')
 def login():
