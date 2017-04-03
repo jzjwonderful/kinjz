1dede88d2b86a418eb97c4f7a497706317d7f066
jared kin <jared_kin@qq.com>
2017-04-04 00:56:54 +0800

    modify login()

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """ import module or package"""
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
 from .forms import LoginForm
 
 @app.route('/')
@@ -17,4 +17,7 @@ def index():
 def login():
     """ setup login route """
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('login success with:' + form.openid.data + 'rember me with:'+str(form.remember_me.data))
+        return redirect('/index')
     return render_template("login.html",title="Login",form=form)
