e700aa97b4adc8129fbad3d090ff7609281c956d
jared kin <jared_kin@qq.com>
2017-04-04 15:15:16 +0800

    add sqlalchemy_track_modifications at config.py

--- config.py
+++ config.py
@@ -17,5 +17,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-
+# disable warning
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 """end sql config"""
